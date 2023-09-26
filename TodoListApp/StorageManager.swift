d2298b9608c1c53f1e765046875481dab2d403d1
Vadim Vanatov <79990006611@yandex.ru>
2023-09-26 17:28:25 +0300

    done

--- TodoListApp/StorageManager.swift
+++ TodoListApp/StorageManager.swift
@@ -5,4 +5,33 @@
 //  Created by HOLY NADRUGANTIX on 26.09.2023.
 //
 
-import Foundation
+import CoreData
+
+final class StorageManager {
+    static let shared = StorageManager()
+    
+    lazy var context = getContext()
+    
+    private init() {}
+    
+    private func getContext() -> NSManagedObjectContext {
+        let container = NSPersistentContainer(name: "TodoListAppStorage")
+        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+            if let error = error as NSError? {
+                fatalError("Unresolved error \(error), \(error.userInfo)")
+            }
+        })
+        return container.viewContext
+    }
+    
+    func saveContext () {
+        if context.hasChanges {
+            do {
+                try context.save()
+            } catch {
+                let nserror = error as NSError
+                fatalError("Unresolved error \(nserror), \(nserror.userInfo)")
+            }
+        }
+    }
+}
--- TodoListApp/TaskListViewController.swift
+++ TodoListApp/TaskListViewController.swift
@@ -8,19 +8,18 @@
 import UIKit
 
 final class TaskListViewController: UITableViewController {
+    // MARK: - Private Properties
     private var taskList: [TodoTask] = []
     private let cellID = "task"
-
+    private let storageManager = StorageManager.shared
+    
+    // MARK: - View Life Cycle Methods
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.register(UITableViewCell.self, forCellReuseIdentifier: cellID)
         view.backgroundColor = .white
         setupNavigationBar()
         fetchData()
-    }
-
-    @objc private func addNewTask() {
-        showAlert(withTitle: "New Task", andMessage: "What do you want to do?")
     }
     
     // MARK: - UITableViewDataSource
@@ -37,48 +36,27 @@ final class TaskListViewController: UITableViewController {
         return cell
     }
     
-    private func fetchData() {
-        guard let appDelegate = (UIApplication.shared.delegate as? AppDelegate) else { return }
-        
-        let fetchRequest = TodoTask.fetchRequest()
-        
-        do {
-            taskList = try  appDelegate.persistentContainer.viewContext.fetch(fetchRequest)
-        } catch {
-            print(error.localizedDescription)
-        }
+    // MARK: - UITableViewDelegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        showAlertWithParameters(
+            title: "Editing \"\(taskList[indexPath.row].title ?? "")\" task",
+            message: "",
+            taskIndexPath: indexPath
+        )
     }
     
-    private func showAlert(withTitle title: String, andMessage message: String) {
-        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
-        let saveAction = UIAlertAction(title: "Save Task", style: .default) { [unowned self] _ in
-            guard let task = alert.textFields?.first?.text, !task.isEmpty else { return }
-            save(task)
+    override func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let action = UIContextualAction(style: .destructive, title: "") { [unowned self] _, _, _ in
+            deleteTask(at: indexPath)
         }
-        let cancelAction = UIAlertAction(title: "Cancel", style: .destructive)
-        alert.addAction(saveAction)
-        alert.addAction(cancelAction)
-        alert.addTextField { textField in
-            textField.placeholder = "New Task"
-        }
-        present(alert, animated: true)
-    }
-    
-    private func save(_ taskName: String) {
-        guard let appDelegate = (UIApplication.shared.delegate as? AppDelegate) else { return }
-        let task = TodoTask(context: appDelegate.persistentContainer.viewContext)
-        task.title = taskName
-        taskList.append(task)
-        
-        let indexPath = IndexPath(row: taskList.count - 1, section: 0)
-        tableView.insertRows(at: [indexPath], with: .automatic)
-        
-        appDelegate.saveContext()
+        action.image = UIImage(systemName: "trash")
+        return UISwipeActionsConfiguration(actions: [action])
     }
 }
 
-// MARK: - Setup UI
+// MARK: - Private Methods
 private extension TaskListViewController {
+    // MARK: - Setup UI
     func setupNavigationBar() {
         title = "Task List"
         navigationController?.navigationBar.prefersLargeTitles = true
@@ -101,5 +79,87 @@ private extension TaskListViewController {
         )
         
         navigationController?.navigationBar.tintColor = .white
+    }
+    
+    // MARK: - Alert Methods
+    func showAlertWithParameters(title: String, message: String, taskIndexPath: IndexPath?) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addTextField { textField in
+            textField.placeholder = "Task Title"
+        }
+        
+        let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) { [unowned self] _ in
+            if let taskIndexPath {
+                tableView.deselectRow(at: taskIndexPath, animated: true)
+            }
+        }
+        let saveAction = UIAlertAction(title: "Save", style: .default) { [unowned self] _ in
+            guard let title = alert.textFields?.first?.text else { return }
+            guard let taskIndexPath else {
+                createTask(withTitle: title)
+                return
+            }
+            updateTask(at: taskIndexPath, withTitle: title)
+        }
+        
+        alert.addAction(cancelAction)
+        alert.addAction(saveAction)
+        
+        let editingChangedAction = UIAction { _ in
+            alert.actions.last?.isEnabled = alert.textFields?.first?.text?.count == 0
+            ? false
+            : true
+        }
+        alert.textFields?.first?.addAction(editingChangedAction, for: .editingChanged)
+        alert.textFields?.first?.returnKeyType = .done
+        alert.textFields?.first?.enablesReturnKeyAutomatically = true
+        alert.textFields?.first?.clearButtonMode = .whileEditing
+        
+        if let taskIndexPath {
+            alert.textFields?.first?.text = taskList[taskIndexPath.row].title
+        } else {
+            alert.actions.last?.isEnabled = false
+        }
+        
+        present(alert, animated: true)
+    }
+    
+    // MARK: - CRUD Methods
+    func createTask(withTitle title: String) {
+        let task = TodoTask(context: storageManager.context)
+        task.title = title
+        taskList.append(task)
+        
+        let indexPath = IndexPath(row: taskList.count - 1, section: 0)
+        tableView.insertRows(at: [indexPath], with: .automatic)
+        
+        storageManager.saveContext()
+    }
+    
+    func fetchData() {
+        let fetchRequest = TodoTask.fetchRequest()
+        do {
+            taskList = try storageManager.context.fetch(fetchRequest)
+        } catch {
+            print(error.localizedDescription)
+        }
+    }
+    
+    func updateTask(at indexPath: IndexPath, withTitle title: String) {
+        taskList[indexPath.row].title = title
+        tableView.reloadRows(at: [indexPath], with: .automatic)
+        storageManager.saveContext()
+    }
+    
+    func deleteTask(at indexPath: IndexPath) {
+        storageManager.context.delete(taskList[indexPath.row])
+        taskList.remove(at: indexPath.row)
+        tableView.deleteRows(at: [indexPath], with: .automatic)
+        storageManager.saveContext()
+    }
+    
+    // MARK: - @objc Selector Methods
+    @objc private func addNewTask() {
+        showAlertWithParameters(title: "Adding a new task", message: "What do you want to do?", taskIndexPath: nil)
     }
 }
